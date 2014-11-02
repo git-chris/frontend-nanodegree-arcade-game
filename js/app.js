63575d871d197411f3da44c05dfb8beca76d87cc
git-chris <chrisbalaf1987@gmail.com>
2014-11-02 13:50:24 +0200

    First draft

--- js/app.js
+++ js/app.js
@@ -1,34 +1,132 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+    this.x = x ;
+    this.y = y ;
+    startx=-150;    
+    this.speed=speed;
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers.
-}
+    // all computers.   
+        this.x = this.x + (1 * this.speed * dt * gemspeed);
+        if (this.x >= 800) {this.x=startx;}    
+};
 
+var gameon=true;//var for pause and continue
+var gemspeed=1;
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
+
+Enemy.prototype.reset=function(){
+   this.x=startx;   
+};
+
+var Gem=function(x,y,speed){
+  this.x=x;
+  this.y=y;
+  startx=-150;
+  this.speed=speed;
+  this.sprite=['images/Gem Blue.png','images/Gem Green.png','images/Gem Orange.png'];
+};
+
+var select=function(array){    
+    return array[Math.floor(Math.random() * array.length)];
+};
+
+
+Gem.prototype.reset=function(){
+  this.x=startx;
+};
+
+Gem.prototype.render=function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Gem.prototype.update=function(){
+    this.x = this.x + (1 * this.speed * dt * gemspeed);
+        if (this.x >= 800) {this.x=startx;} 
+};
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player=function(){
+    this.sprite='images/char-boy.png';    
+    this.x=700;
+    this.y=60;
+    this.lives=3;
+};
 
+Player.prototype.render=function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+Player.prototype.handleInput=function(key){      
+     if(gameon){ 
+        switch(key){           
+            case 'left':/*If statement sets boundaries to left movement*/
+               if(!(this.x<20)){               
+                this.x=this.x-101;
+                };
+            break;
+        case 'right':/*Same as above*/
+                if(!(this.x>690)){                
+                this.x=this.x+101;
+                };
+            break;
+        case 'up':
+                this.y=this.y-84;               
+            break;
+        case 'down':
+                this.y=this.y+84;                
+            break;        
+    case 'pause':
+                gameon=false;
+                alert("Game paused! to continue press OK!");
+                gameon=true;
+            break;
+            ;}
+        };
+};
+       
+Player.prototype.reset = function() {
+    this.x=700;
+    this.y=60;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+
+
+
+var allEnemies=[];
+var createEnemies=function(){
+    allEnemies.push(new Enemy(-150,60,23));
+    allEnemies.push(new Enemy(-150,228,42));
+    allEnemies.push(new Enemy(-150,144,37));
+    allEnemies.push(new Enemy(-150,60,50));
+    allEnemies.push(new Enemy(-150,312,88));
+};
+
+var gems=[];
+var player=new Player();
+var bluegems=0;
 
 
 
@@ -39,8 +137,8 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        80: 'pause', /*added p for pause button*/        
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -6,79 +6,160 @@ var Engine = (function(global) {
         patterns = {},
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    canvas.width = 807;
+    canvas.height = 800;
     doc.body.appendChild(canvas);
 
-    function main() {
+    
+    function main() {         
         var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
-
-        update(dt);
-        render();
-
+            dt = (now - lastTime) / 1000.0;       
+        if(gameon){             
+           update(dt);
+        };
+        render();        
         lastTime = now;
-        win.requestAnimationFrame(main);
+        win.requestAnimationFrame(main);        
+    };
+    
+    
+    function start(){        
+       gameon=false;
+       $('#dialog').dialog();
+       $('html').click(function() {
+        gameon=true;
+       });
+    };
+           
+    
+    function init() {       
+        lastTime = Date.now();
+        start();        
+        main();     
+        createEnemies();        
+    }
+
+    function update(dt) {        
+       updateEntities(dt);
+       checkCollisions(dt);
+       drown();      
+    };    
+    
+    function drown(){
+        y=player.y;
+        if(y<60 | y>312){
+            new Audio("bubbling1.wav").play();
+            alert('You cannot swim! Ok?');
+            reset()};    
+    };
+    
+    function checkCollisions(){
+        var y=player.y;
+        var x=player.x;        
+        allEnemies.forEach(function(enemy) {
+           if(enemy.y==y){ 
+             if(enemy.x+50>x & x+50>enemy.x){                             
+               reset();
+            };            
+           ;}       
+        });        
+    };
+    
+    function pickgems(){
+      greengems.forEach(function(gem) {
+            if(gem.x==player.x&&gem.y==player.y){
+              gemspeed*=2;  
+            };            
+        });
     };
 
-    function init() {
-
-        reset();
-        lastTime = Date.now();
-        main();
-    }
-
-    function update(dt) {
-        updateEntities(dt);
-        // checkCollisions();
-    }
-
-    function updateEntities(dt) {
+    function updateEntities(dt) {       
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
-        });
-        player.update();
-    }
+        });   
+               
+    };
 
     function render() {
         var rowImages = [
                 'images/water-block.png',
                 'images/stone-block.png',
+                'images/grass-block.png',                
+                'images/grass-block.png',                
                 'images/stone-block.png',
-                'images/stone-block.png',
-                'images/grass-block.png',
-                'images/grass-block.png'
+                'images/water-block.png'
             ],
             numRows = 6,
-            numCols = 5,
+            numCols = 8,
             row, col;
 
         for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
+            for (col = 0; col < numCols; col++) {                 
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-            }
-        }
-
+            };
+        };
         renderEntities();
-    }
+    };
 
     function renderEntities() {
         allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
-        player.render();
-    }
+            enemy.render();            
+        });        
+        player.render(); 
+        renderlives();
+        renderdiamonds();
+        
+    };
+        
+    function renderlives(){       
+        ctx.rect(10, 600, 150, 40);
+        ctx.fillStyle=('#81ccee');
+        ctx.fill();
+        var x=10;
+        var li=player.lives;        
+        for(i=0;i<li;i++){           
+            ctx.drawImage(Resources.get('images/rsheart.png'),x,600);
+            x+=40;
+        };      
+    };
+    
+    function renderdiamonds(){
+        ctx.drawImage(Resources.get('images/rsz_gem_blue.png'),15,2);        
+        ctx.fillStyle=('black');
+        ctx.font=('20px Verdana');        
+        ctx.fillText(':'+bluegems,60,40);
+    };
 
-    function reset() {
-        // noop
-    }
-
+    function reset() {        
+         allEnemies.forEach(function(enemy) {
+            enemy.reset();
+        });       
+        player.reset();
+        player.lives-=1;
+        if(!player.lives){gameover();};
+        renderlives();
+    };
+    
+    function gameover(){  
+        new Audio("buzzer_x.wav").play();
+        ctx.fillStyle=('black');
+        ctx.font=('80px Verdana');        
+        ctx.fillText('GAME OVER...',200,500);
+        canvas.freeze();        
+    };
+    
+    
     Resources.load([
         'images/stone-block.png',
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/rsheart.png',
+        'images/Star.png',
+        'images/rsz_gem_blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png'
     ]);
     Resources.onReady(init);
 
