f30959b79502685de86400f30c1aff5ac753d394
git-chris <chrisbalaf1987@gmail.com>
2014-11-14 17:35:06 +0200

    2nd Submision
    Js code format corrections and Updated README

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 // Enemies our player must avoid
 //random speed and starting point with select()
-var Enemy = function() {    
+var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
-    this.x=select([-140,-120,-220,-200]);
-    this.y=select([60,144,228,312]);       
-    this.speed=select([13,25,44,55,36,42]);
+    this.x = select([-140, -120, -220, -200]);
+    this.y = select([60,144,228,312]);
+    this.speed = select([13,25,44,55,36,42]);
 };
 
 // Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks 
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers. 
+    // all computers.
     //!I added gemspeed. Increases by picking orange gems
-        this.x = this.x + (1 * this.speed * dt + gemspeed);
-        if (this.x >= 800) {remove(this,allEnemies);createEnemies();}    
+    this.x = this.x + (this.speed * dt + gemSpeed);
+    if (this.x >= 800){remove(this,allEnemies);createEnemies();}    
 };
 
-var gameon=true; //var for pause and continue
-var gemspeed=1;
+var gameon = true; //var for pause and continue
+var gemSpeed = 1;
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -28,77 +28,77 @@ Enemy.prototype.render = function() {
 
 //When a bug resets either by player loss or by reaching end of canvas
 //is destroyed and recreated at random starting point
-Enemy.prototype.reset=function(){
-   allEnemies.forEach(function(enemy){remove(enemy,allEnemies)});
-   createEnemies();
+Enemy.prototype.reset = function() {
+    allEnemies.forEach(function(enemy){remove(enemy,allEnemies);});
+    createEnemies();
 };
 
 //Destroying from gameplay picked up Gems and Enemies
 var remove = function (entity, array) {
-            var index = array.indexOf(entity);
+    var index = array.indexOf(entity);
             if (index !== -1) {
             array.splice(index, 1);
-            }
+            };
 };
 
-var Gem=function(){
-  this.x=select([-150,-180,-200]);
-  this.y=select([60,144,228,312]);
-  startx=-150;
-  this.speed=select([10,20,30,40,12,55,23,38,15]);
-  this.sprite=select(['images/Gem Blue.png','images/Gem Orange.png','images/Gem Green.png']);
+var Gem = function() {
+    this.x = select([-150, -180, -200]);
+    this.y = select([60,144,228,312]);
+    startx = -150;
+    this.speed = select([10,20,30,40,12,55,23,38,15]);
+    this.sprite = select(['images/Gem Blue.png','images/Gem Orange.png','images/Gem Green.png']);
 };
 
-var select=function(array){    
+var select = function(array) {
     return array[Math.floor(Math.random() * array.length)];
 };
 
 //Gems return to start x position. Not destroyed until picked up!
-Gem.prototype.reset=function(){
-  this.x=startx;
+Gem.prototype.reset = function() {
+    this.x = startx;
 };
 
-Gem.prototype.render=function(){
+Gem.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Gem.prototype.update=function(dt){
-    this.x = this.x + (1 * this.speed * dt * gemspeed);
-        if (this.x >= 800) {this.x=startx;} 
+Gem.prototype.update = function(dt) {
+    this.x = this.x + (this.speed * dt * gemSpeed);
+        if (this.x >= 800){this.x = startx;}
 };
 
-var Player=function(){
-    this.sprite='images/char-boy.png';    
-    this.x=700; //fixed x axis posotion
-    this.y=60;  //fixed y axis position
-    this.lives=3;
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 700; //fixed x axis posotion
+    this.y = 60;  //fixed y axis position
+    this.lives = 3;
 };
 
-Player.prototype.render=function(){
+Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput=function(key){      
-     if(gameon){ //disables key response if game paused
-        switch(key){           
+Player.prototype.handleInput = function(key) {
+    if(gameon){ //disables key response if game paused
+        switch(key) {
             case 'left':/*If statement sets boundaries to left movement*/
-               if(!(this.x<20)){               
-                this.x=this.x-101;
+               if(!(this.x<20)){
+                this.x = this.x-101;
                 };
             break;
         case 'right':/*Same as above*/
-                if(!(this.x>690)){                
-                this.x=this.x+101;
+                if(!(this.x>690)){
+                this.x = this.x+101;
                 };
             break;
         case 'up':
-                this.y=this.y-84;               
+                this.y = this.y-84;
             break;
         case 'down':
-                this.y=this.y+84;                
-            break;        
-    case 'pause':
-                gameon=false;
+                this.y = this.y+84;
+            break;
+        case 'pause':
+                gameon = false;
                 swal({
                 title: "Game Paused",
                 text: "Press Enter to Resume",
@@ -107,16 +107,16 @@ Player.prototype.handleInput=function(key){
                 confirmButtonColor: "#DD6B55",
                 confirmButtonText: "Resume!",
                 closeOnConfirm: true
-                }),function(){               
+                }),function() {
                 gameon = true;};
             break;
             ;}
         };
 };
-       
+
 Player.prototype.reset = function() {
-    this.x=700;
-    this.y=60;
+    this.x = 700;
+    this.y = 60;
 };
 
 // Now instantiate your objects.
@@ -124,18 +124,18 @@ Player.prototype.reset = function() {
 // Place the player object in a variable called player
 // Place Gems in gems array
 // bluegems and greengems are for score and lives respectively
-var allEnemies=[];
-var createEnemies=function(){
-   while(allEnemies.length<3){allEnemies.push(new Enemy())};
+var allEnemies =[];
+var createEnemies = function() {
+    while(allEnemies.length<3){allEnemies.push(new Enemy());};
 };
 
-var gems=[];
-var creategems=function(){
-   while(gems.length<2){gems.push(new Gem())};
+var gems =[];
+var createGems = function() {
+    while(gems.length<2){gems.push(new Gem());};
 };
-var player=new Player();
-var bluegems=0;
-var greengems=0;
+var player = new Player();
+var blueGems = 0;
+var greenGems = 0;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -145,7 +145,7 @@ document.addEventListener('keyup', function(e) {
         38: 'up',
         39: 'right',
         40: 'down',
-        80: 'pause', /*added p for pause button*/        
+        80: 'pause' /*added p for pause button*/
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -9,66 +9,66 @@ var Engine = (function(global) {
     canvas.width = 807;
     canvas.height = 700;
     doc.body.appendChild(canvas);
- 
-    function main() {         
+
+    function main() {
         var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;  
-            /*With gameon var I am pausing the game*/
-        if(gameon){             
-           update(dt);
+            dt = (now - lastTime) / 1000.0;
+        /**With gameon var I am pausing the game*/
+        if(gameon){
+            update(dt);
         };
-        render();        
+        render();
         lastTime = now;
-        win.requestAnimationFrame(main);        
+        win.requestAnimationFrame(main);
     };
-      /*Start  messages with instructions*/  
-    function start(){        
-       gameon=false;
-       swal({
+    /**Start  messages with instructions*/
+    function start() {
+        gameon = false;
+        swal({
             title: "Welcome",
             text: "This is my version of Frogger. Press ENTER to Continue!",
             type: "info",
-            showCancelButton: false,            
+            showCancelButton: false,
             confirmButtonText: "Next!",
             closeOnConfirm: false},
-            function(){
+            function() {
                 swal({title:"Objectives",
-                      text:"Collect as many Blue gems as you can.  \n\
-                            Collect 4 Green and Earn an Extra Life.\n\
-                            Orange gems are special! ",
-                      confirmButtonText: "Next!",
-                      closeOnConfirm: false},
-                    function(){    
-                        swal({title:"Instructions",
-                              text:"Use Arrow Keys for Movement. \n\
-                                    Key P for Pausing.\n\
-                                    Press Enter to Start the Game.",
-                              confirmButtonText: "Start!"},
-                            function(){gameon=true;});
+                        text:"Collect as many Blue gems as you can.  \n\
+                              Collect 4 Green and Earn an Extra Life.\n\
+                              Orange gems are special! ",
+                        confirmButtonText: "Next!",
+                        closeOnConfirm: false},
+                        function() {
+                            swal({title:"Instructions",
+                                text:"Use Arrow Keys for Movement. \n\
+                                      Key P for Pausing.\n\
+                                      Press Enter to Start the Game.",
+                                confirmButtonText: "Start!"},
+                                function() {gameon = true;});
                     });
             });
-    };           
-    /*Initialize the game by creating Entities*/
-    function init() {       
+    };
+    /**Initialize the game by creating Entities*/
+    function init() {
         lastTime = Date.now();
-        start();        
-        main();     
+        start();
+        main();
         createEnemies();
         creategems();
     }
-    /*In the update I check also for player boundaries on y axis with drown function*/
-    function update(dt) {        
-       updateEntities(dt);
-       checkCollisions();
-       pickgems();       
-       drown();      
-    };    
-    
-    function drown(){
-        y=player.y;
+    /**In the update I check also for player boundaries on y axis with drown function*/
+    function update(dt) {
+        updateEntities(dt);
+        checkCollisions();
+        pickGems();
+        drown();
+    };
+
+    function drown() {
+        y = player.y;
         if(y<60 | y>312){
             new Audio("audio/bubbling1.wav").play();
-            gameon=false;
+            gameon = false;
             swal({
                 title: "You Just Drowned!",
                 text: "Press Enter to Resume",
@@ -77,58 +77,58 @@ var Engine = (function(global) {
                 confirmButtonColor: "#DD6B55",
                 confirmButtonText: "Resume!",
                 closeOnConfirm: true
-                },function(){gameon=true;});
+                },function() {gameon = true;});
                 reset();
-        };    
+        };
     };
-    
-    function checkCollisions(){
-        var y=player.y;
-        var x=player.x;        
-        allEnemies.forEach(function(enemy) {
-           if(enemy.y==y){               
-             if(enemy.x+50>x & x+50>enemy.x){   //By checking the distance player x coordinate has from right and left we have collision
-                 new Audio("audio/punch.mp3").play();
-                gameon=false;
-                swal({
-                title: "A Bug Just Ate You!",
-                text: "Press Enter to Resume",
-                type: "warning",
-                showCancelButton: false,
-                confirmButtonColor: "#DD6B55",
-                confirmButtonText: "Resume!",
-                closeOnConfirm: true
-                },function(){gameon=true;});
-                reset();           
-            };        
-           };       
-        });        
-    };  
-    
-    function pickgems() {
-      gems.forEach(function (gem) {
-      if (player.x<gem.x+50 && player.x>gem.x-50 && player.y === gem.y) {
-        if (gem.sprite=='images/Gem Blue.png') {bluegems+=1;remove(gem,gems);}
-        else if (gem.sprite=='images/Gem Orange.png'){gemspeed+=1;remove(gem,gems);}
-        else if (gem.sprite=='images/Gem Green.png'){greengems+=1;remove(gem,gems);}           
-           }; 
-           extralives(); //Right after picking a gem checking if greengems reached 4
-           creategems(); // Create missing gems from gameplay
-           renderscore();
-      });
-    };     
 
-    function updateEntities(dt) {       
-        allEnemies.forEach(function(enemy){enemy.update(dt);});
-        gems.forEach(function(gem){gem.update(dt);});
+    function checkCollisions() {
+        var y = player.y;
+        var x = player.x;
+        allEnemies.forEach(function(enemy) {
+            if(enemy.y == y){
+                if(enemy.x+50>x & x+50>enemy.x) {   //By checking the distance player x coordinate has from right and left we have collision
+                    new Audio("audio/punch.mp3").play();
+                    gameon = false;
+                    swal({
+                    title: "A Bug Just Ate You!",
+                    text: "Press Enter to Resume",
+                    type: "warning",
+                    showCancelButton: false,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Resume!",
+                    closeOnConfirm: true
+                    },function() {gameon = true;});
+                    reset();
+            };
+           };
+        });
+    };
+
+    function pickGems() {
+        gems.forEach(function (gem) {
+        if (player.x<gem.x+50 && player.x>gem.x-50 && player.y === gem.y){
+            if (gem.sprite == 'images/Gem Blue.png'){blueGems += 1;remove(gem,gems);}
+            else if (gem.sprite == 'images/Gem Orange.png'){gemSpeed += 1;remove(gem,gems);}
+            else if (gem.sprite == 'images/Gem Green.png'){greenGems += 1;remove(gem,gems);}
+           };
+        extraLives(); //Right after picking a gem checking if greengems reached 4
+        createGems(); // Create missing gems from gameplay
+        renderScore();
+      });
+    };
+
+    function updateEntities(dt) {
+        allEnemies.forEach(function(enemy) {enemy.update(dt);});
+        gems.forEach(function(gem) {gem.update(dt);});
     };
 
     function render() {
         var rowImages = [
                 'images/water-block.png',
                 'images/stone-block.png',
-                'images/grass-block.png',                
-                'images/grass-block.png',                
+                'images/grass-block.png',
+                'images/grass-block.png',
                 'images/stone-block.png',
                 'images/water-block.png'
             ],
@@ -137,8 +137,8 @@ var Engine = (function(global) {
             row, col;
 
         for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {                 
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);                
+            for (col = 0; col < numCols; col++) {
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
             };
         };
         renderEntities();
@@ -146,70 +146,71 @@ var Engine = (function(global) {
 
     function renderEntities() {
         allEnemies.forEach(function(enemy) {
-            enemy.render();            
+            enemy.render();
         });
         gems.forEach(function(gem) {
-            gem.render();            
-        });       
-        player.render(); 
-        renderlives();
-        renderscore();
-    };
-        
-    function renderlives(){       
-        ctx.rect(10, 600, 200, 40);
-        ctx.fillStyle=('#81ccee');
-        ctx.fill();
-        var x=10;
-        var li=player.lives;        
-        for(i=0;i<li;i++){           
-            ctx.drawImage(Resources.get('images/rsheart.png'),x,600);
-            x+=40;
-        };      
-    };
-    
-    function extralives(){
-      if (greengems===4){
-        new Audio("audio/Ta Da.mp3").play();
-        player.lives+=1;
-        renderlives();
-        greengems=0;}
-    };
-    
-    function renderscore(){
-        ctx.fillStyle=('#81ccee');
-        ctx.fillRect(15,2,180,45);
-        ctx.drawImage(Resources.get('images/rsz_gem_blue.png'),15,2);       
-        ctx.fillStyle=('black');
-        ctx.font=('20px Verdana');        
-        ctx.fillText(':'+bluegems,60,40);
-        ctx.drawImage(Resources.get('images/rsz_gem_green.png'),110,2);
-        ctx.fillText(':'+greengems,155,40);
+            gem.render();
+        });
+        player.render();
+        renderLives();
+        renderScore();
     };
 
-    function reset() {        
+    function renderLives() {
+        ctx.rect(10, 600, 200, 40);
+        ctx.fillStyle = ('#81ccee');
+        ctx.fill();
+        var x = 10;
+        var li = player.lives;
+        for(i=0;i<li;i++){
+            ctx.drawImage(Resources.get('images/rsheart.png'),x,600);
+            x += 40;
+        };
+    };
+
+    function extraLives() {
+        if (greenGems === 4){
+            new Audio("audio/Ta Da.mp3").play();
+            player.lives += 1;
+            renderLives();
+            greenGems = 0;
+        };
+    };
+
+    function renderScore() {
+        ctx.fillStyle = ('#81ccee');
+        ctx.fillRect(15,2,180,45);
+        ctx.drawImage(Resources.get('images/rsz_gem_blue.png'),15,2);
+        ctx.fillStyle = ('black');
+        ctx.font = ('20px Verdana');
+        ctx.fillText(':'+blueGems,60,40);
+        ctx.drawImage(Resources.get('images/rsz_gem_green.png'),110,2);
+        ctx.fillText(':'+greenGems,155,40);
+    };
+
+    function reset() {
          allEnemies.forEach(function(enemy) {
             enemy.reset();
-        });       
+        });
         player.reset();
-        player.lives-=1;
+        player.lives -= 1;
         if(!player.lives){gameover();};
-        renderlives();        
+        renderLives();
     };
-    
-    //Game ends. Canvas freezes and needs restart
-    function gameover(){
+
+    /**Game ends. Canvas freezes and needs restart*/
+    function gameover() {
         ctx.rect(10, 600, 150, 40);
-        ctx.fillStyle=('#81ccee');
+        ctx.fillStyle = ('#81ccee');
         ctx.fill();
         new Audio("audio/buzzer_x.wav").play();
-        ctx.fillStyle=('black');
-        ctx.font=('80px Verdana');        
+        ctx.fillStyle = ('black');
+        ctx.font = ('80px Verdana');
         ctx.fillText('GAME OVER...',150,400);
         canvas.freeze();
     };
-    
-    
+
+
     Resources.load([
         'images/stone-block.png',
         'images/water-block.png',
